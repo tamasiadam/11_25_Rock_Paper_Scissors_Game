fcafa519a3bf2e24c4a09a87481adfcb784a5d43
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2024-11-25 10:20:38 +0100

    ora vege

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,5 +1,43 @@
 import random
 
-choices = ["rock", "paper", "scissors"]
+choices = ["kő", "papír", "olló"]
+machine_choice = random.choice(choices)
+gyozelem = 0
+vereseg = 0
+dontetlen = 0
+jatek = True
+print("Üdvözöllek! Ez itt a Kő, Papír, Olló játék!")
+jatekos = input("Kérlek válassz, Kő, papír, olló? ")
 
-# machine_choice = random.choice(choices)
+print(f"A választásod: {jatekos}")
+print(f"A gép választása: {machine_choice}")
+while jatek == True:
+    if machine_choice == jatekos:
+        print("Döntetlen")
+        dontetlen += 1
+    elif machine_choice == "kő" and jatekos == "papír":
+        print("Győzelem!")
+        gyozelem += 1
+    elif machine_choice == "kő" and jatekos == "olló":
+        print("Vereség")
+        vereseg += 1
+    elif machine_choice == "papír" and jatekos == "olló":
+        print("Győzelem!")
+        gyozelem += 1
+    elif machine_choice == "papír" and jatekos == "kő":
+        print("Vereség!")
+        vereseg += 1
+    elif machine_choice == "olló" and jatekos == "kő":
+        print("Győzelem!")
+        gyozelem += 1
+    elif machine_choice == "olló" and jatekos == "papír":
+        print("Vereség!")
+        vereseg += 1
+
+    print(f"Győzelem: {gyozelem}\nVereség: {vereseg}\nDöntetlen: {dontetlen}")
+    again = input("Akarsz még játszani? (i/n) ")
+    if again == "n":
+        jatek = False
+    elif again == ""
+        print()
+        
